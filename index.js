457e0ab840c4d061e09eb981728b2cab5aba83dc
unknown <caglabircan@hotmail.com>
2022-11-05 02:16:17 +0300

    Commpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+/*   function superbowlWin(record) {
+
+    if (record.find(element => element.result === "w")) {
+        return result.year
+    } else {
+        return undefined
+    }
+    } */
+
+
+    function superbowlWin(record){
+        const result = record.find(item => item.result === 'W')
+    
+       if (result){
+            return result.year
+        }else{
+            return undefined
+    }
+    }
+    
+    
+    
